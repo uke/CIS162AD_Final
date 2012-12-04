04ba1b39d42bc1067c4096aa5d18bae2be8f6a59
uke <iamuke@gmail.com>
2012-12-04 15:38:22 -0700

    Filling in event handlers for main menu

--- FinalProject/Form1.Designer.cs
+++ FinalProject/Form1.Designer.cs
@@ -31,27 +31,27 @@
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
-            this.menuItem1 = new System.Windows.Forms.MenuItem();
-            this.menuItem2 = new System.Windows.Forms.MenuItem();
-            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItemFile = new System.Windows.Forms.MenuItem();
+            this.menuItemFileClear = new System.Windows.Forms.MenuItem();
+            this.menuItemFileExit = new System.Windows.Forms.MenuItem();
+            this.menuItemView = new System.Windows.Forms.MenuItem();
+            this.menuItemViewAccountInformation = new System.Windows.Forms.MenuItem();
+            this.menuItemViewActivity = new System.Windows.Forms.MenuItem();
+            this.menuItemHelp = new System.Windows.Forms.MenuItem();
             this.menuItemAbout = new System.Windows.Forms.MenuItem();
-            this.menuItem5 = new System.Windows.Forms.MenuItem();
-            this.menuItem6 = new System.Windows.Forms.MenuItem();
-            this.menuItem7 = new System.Windows.Forms.MenuItem();
-            this.menuItem8 = new System.Windows.Forms.MenuItem();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -84,12 +84,78 @@
             this.tabPage1.Text = "Account Information";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Continue";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Beginning Balance";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Account Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Account Name";
+            // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(560, 173);
+            this.tabPage2.Size = new System.Drawing.Size(560, 247);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Activity";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -124,100 +190,56 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "ROB2081807";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(25, 18);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 13);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Account Name";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(118, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(172, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 53);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 13);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Account Number";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(118, 50);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(172, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 88);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(96, 13);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Beginning Balance";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(118, 85);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(172, 20);
-            this.textBox3.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(118, 177);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Continue";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(215, 177);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // mainMenu1
             // 
             this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-            this.menuItem1,
-            this.menuItem2,
-            this.menuItem3,
+            this.menuItemFile,
+            this.menuItemView,
+            this.menuItemHelp,
             this.menuItemAbout});
             // 
-            // menuItem1
+            // menuItemFile
             // 
-            this.menuItem1.Index = 0;
-            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-            this.menuItem5,
-            this.menuItem6});
-            this.menuItem1.Text = "&File";
+            this.menuItemFile.Index = 0;
+            this.menuItemFile.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItemFileClear,
+            this.menuItemFileExit});
+            this.menuItemFile.Text = "&File";
             // 
-            // menuItem2
+            // menuItemFileClear
             // 
-            this.menuItem2.Index = 1;
-            this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-            this.menuItem7,
-            this.menuItem8});
-            this.menuItem2.Text = "&View";
+            this.menuItemFileClear.Index = 0;
+            this.menuItemFileClear.Text = "&Clear";
+            this.menuItemFileClear.Click += new System.EventHandler(this.menuItemClear_Click);
             // 
-            // menuItem3
+            // menuItemFileExit
             // 
-            this.menuItem3.Index = 2;
-            this.menuItem3.Text = "&Help";
+            this.menuItemFileExit.Index = 1;
+            this.menuItemFileExit.Text = "E&xit";
+            this.menuItemFileExit.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // menuItemView
+            // 
+            this.menuItemView.Index = 1;
+            this.menuItemView.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItemViewAccountInformation,
+            this.menuItemViewActivity});
+            this.menuItemView.Text = "&View";
+            // 
+            // menuItemViewAccountInformation
+            // 
+            this.menuItemViewAccountInformation.Index = 0;
+            this.menuItemViewAccountInformation.Text = "Account &Information";
+            // 
+            // menuItemViewActivity
+            // 
+            this.menuItemViewActivity.Index = 1;
+            this.menuItemViewActivity.Text = "&Activity";
+            // 
+            // menuItemHelp
+            // 
+            this.menuItemHelp.Index = 2;
+            this.menuItemHelp.Text = "&Help";
             // 
             // menuItemAbout
             // 
@@ -225,27 +247,6 @@
             this.menuItemAbout.Shortcut = System.Windows.Forms.Shortcut.F1;
             this.menuItemAbout.Text = "&About";
             this.menuItemAbout.Click += new System.EventHandler(this.menuItemAbout_Click);
-            // 
-            // menuItem5
-            // 
-            this.menuItem5.Index = 0;
-            this.menuItem5.Text = "&Clear";
-            // 
-            // menuItem6
-            // 
-            this.menuItem6.Index = 1;
-            this.menuItem6.Text = "E&xit";
-            this.menuItem6.Click += new System.EventHandler(this.menuItem6_Click);
-            // 
-            // menuItem7
-            // 
-            this.menuItem7.Index = 0;
-            this.menuItem7.Text = "Account &Information";
-            // 
-            // menuItem8
-            // 
-            this.menuItem8.Index = 1;
-            this.menuItem8.Text = "&Activity";
             // 
             // Form1
             // 
@@ -284,13 +285,13 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MainMenu mainMenu1;
-        private System.Windows.Forms.MenuItem menuItem1;
-        private System.Windows.Forms.MenuItem menuItem5;
-        private System.Windows.Forms.MenuItem menuItem6;
-        private System.Windows.Forms.MenuItem menuItem2;
-        private System.Windows.Forms.MenuItem menuItem7;
-        private System.Windows.Forms.MenuItem menuItem8;
-        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItemFile;
+        private System.Windows.Forms.MenuItem menuItemFileClear;
+        private System.Windows.Forms.MenuItem menuItemFileExit;
+        private System.Windows.Forms.MenuItem menuItemView;
+        private System.Windows.Forms.MenuItem menuItemViewAccountInformation;
+        private System.Windows.Forms.MenuItem menuItemViewActivity;
+        private System.Windows.Forms.MenuItem menuItemHelp;
         private System.Windows.Forms.MenuItem menuItemAbout;
     }
 }
--- FinalProject/Form1.cs
+++ FinalProject/Form1.cs
@@ -20,14 +20,19 @@ namespace FinalProject
         {
             MessageBox.Show("CIS162AD Final Project -- Bank Of Bartlett\n\n"
                 + "By Robert Bartlett, MEID ROB2081807, 2012-12-7\n\n"
-                + "This app simulates a bank register.  Thought I'd spif it up just a tad.",
+                + "This app simulates a bank register.  Thought I'd spiff it up just a tad.",
                 "About CIS162AD Final Project", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void menuItem6_Click(object sender, EventArgs e)
+        private void menuItemExit_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Are you sure you want to quit the Bank of Bartlett application? Nothing will be saved and you will be left penniless with only a dog as a friend.", "Quit Bank of Bartlett?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+            if (MessageBox.Show("Are you sure you want to quit the Bank of Bartlett application? Nothing will be saved and you will be left penniless with only a smelly half-blind dog as a friend.", "Quit Bank of Bartlett?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
                 Environment.Exit(0);
+        }
+
+        private void menuItemClear_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clear!");
         }
     }
 }
